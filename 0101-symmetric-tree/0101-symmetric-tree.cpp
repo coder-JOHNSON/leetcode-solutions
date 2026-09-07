9ad493fff6c6009568170ea1c0a58e3dfc957ce5
coder-JOHNSON <jy9871023@gmail.com>
2026-09-07 12:27:16 +0530

    Time: 0 ms (100%), Space: 18.6 MB (14.06%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,25 +11,37 @@
  */
 class Solution {
 public: 
-    void invert(TreeNode *root){
-        if(root == NULL) return;
-        TreeNode *temp = root -> left;
-        root -> left = root -> right;
-        root -> right = temp;
-        invert(root -> left);
-        invert(root -> right);
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == nullptr && q == nullptr) return true;
-        if(p == nullptr || q == nullptr) return false;
+    // void invert(TreeNode *root){
+    //     if(root == NULL) return;
+    //     TreeNode *temp = root -> left;
+    //     root -> left = root -> right;
+    //     root -> right = temp;
+    //     invert(root -> left);
+    //     invert(root -> right);
+    // }
+    // bool isSameTree(TreeNode* p, TreeNode* q) {
+    //     if(p == nullptr && q == nullptr) return true;
+    //     if(p == nullptr || q == nullptr) return false;
          
+    //     if(p -> val != q -> val) return false;
+    //     return isSameTree(p -> left,q -> left) && isSameTree(p -> right,q -> right);
+    // }
+    // bool isSymmetric(TreeNode* root) {
+    //     invert(root -> right);
+    //     bool ans = isSameTree(root -> left,root -> right);
+    //     invert(root -> right);
+    //     return ans;
+    // }
+
+    bool areInverts(TreeNode *p, TreeNode *q){
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
         if(p -> val != q -> val) return false;
-        return isSameTree(p -> left,q -> left) && isSameTree(p -> right,q -> right);
+        
+        return areInverts(p -> left,q -> right) && areInverts(p -> right,q -> left);
     }
+
     bool isSymmetric(TreeNode* root) {
-        invert(root -> right);
-        bool ans = isSameTree(root -> left,root -> right);
-        invert(root -> right);
-        return ans;
+        return areInverts(root -> left, root -> right);
     }
 };
