085ff669f3d858495398156e5f109436d2178a83
coder-JOHNSON <jy9871023@gmail.com>
2026-09-07 14:35:35 +0530

    Time: 0 ms (100%), Space: 14.8 MB (96.16%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,21 +11,37 @@
  */
 class Solution {
 public:
+    // vector<int> rightSideView(TreeNode* root) {
+    // queue<TreeNode*> dq;
+    // vector<int> v;
+    // dq.push(root);
+    // if(root == nullptr) return v;
+    //     while(dq.size()){
+    //         int n = dq.size();
+    //         for(int i = 0; i < n; i++){
+    //             TreeNode *temp = dq.front();
+    //             dq.pop();
+    //             if(temp -> left != nullptr) dq.push(temp -> left);
+    //             if(temp -> right != nullptr) dq.push(temp -> right);
+    //             if(i == n - 1) v.push_back(temp -> val);
+    //         }
+    //     }
+    //     return v;
+
+    int levels(TreeNode *root){
+        if(root == NULL) return 0;
+        return 1 + max(levels(root -> left),levels(root -> right));
+    }
+    void preorder(TreeNode *root,int lvl, vector<int> &ans){
+        if(root == NULL) return;
+        ans[lvl] = root -> val;
+        preorder(root -> left,lvl + 1,ans);
+        preorder(root -> right,lvl + 1,ans);
+    }
     vector<int> rightSideView(TreeNode* root) {
-    queue<TreeNode*> dq;
-    vector<int> v;
-    dq.push(root);
-    if(root == nullptr) return v;
-        while(dq.size()){
-            int n = dq.size();
-            for(int i = 0; i < n; i++){
-                TreeNode *temp = dq.front();
-                dq.pop();
-                if(temp -> left != nullptr) dq.push(temp -> left);
-                if(temp -> right != nullptr) dq.push(temp -> right);
-                if(i == n - 1) v.push_back(temp -> val);
-            }
-        }
-        return v;
+       int n = levels(root);
+       vector<int> ans(n);
+       preorder(root,0,ans);
+       return ans;
     }
 };
